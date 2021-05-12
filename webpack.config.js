9e8b83f6efc894b592897865ba9b6e3b40a22b7b
Anthony North <anthony.jl.north@gmail.com>
2021-05-13 09:43:46 +1000

    minimal webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,29 +5,24 @@ module.exports = (env, { mode }) => {
 
   return {
     mode,
-    entry: { rdeckControls: "./src" },
+    entry: { rdeckControls: "./src/rdeckControls" },
     output: {
       path: resolve(__dirname, "inst/htmlwidgets"),
       filename: "[name].js",
       library: "rdeckControls",
       libraryTarget: "umd",
     },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js", "jsx"],
-      mainFields: ["esnext", "browser", "module", "main"],
-    },
+    resolve: { extensions: [".ts", ".js"] },
     module: {
       rules: [
         {
           test: /\.(ts|js)x?$/,
           exclude: /node_modules/,
           loader: "ts-loader",
-        }
+        },
       ],
     },
     devtool: mode === "development" && "inline-source-map",
-    optimization: {
-      minimize: mode === "production",
-    },
+    optimization: { minimize: mode === "production" },
   };
 };
